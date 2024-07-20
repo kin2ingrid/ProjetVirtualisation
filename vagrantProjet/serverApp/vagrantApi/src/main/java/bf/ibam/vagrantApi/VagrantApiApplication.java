f30a2a5d4b336c81835c5af5a449a442350b0484
dkrach <niyiobo@live.fr>
2024-07-20 00:43:52 +0000

    correction du package

--- vagrantProjet/serverApp/vagrantApi/src/main/java/bf/ibam/vagrantApi/VagrantApiApplication.java
+++ vagrantProjet/serverApp/vagrantApi/src/main/java/bf/ibam/vagrantApi/VagrantApiApplication.java
@@ -2,8 +2,10 @@ package bf.ibam.vagrantApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class VagrantApiApplication {
 
 	public static void main(String[] args) {
