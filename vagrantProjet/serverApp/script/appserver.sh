e4abb4f40628361b7211f4b87ebbd7134f26fc5e
dkrach <niyiobo@live.fr>
2024-07-22 18:46:26 +0000

    end of implementation

--- vagrantProjet/serverApp/script/appserver.sh
+++ vagrantProjet/serverApp/script/appserver.sh
@@ -5,14 +5,18 @@ sudo apt-get update
 # Installation de Java (JDK 17)
 sudo apt-get install -y wget
 sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
-sudo apt install -y ./jdk-17_linux-x64_bin.deb
+sudo apt install -y ./jdk-17_linux-x64_bin.deb maven dos2unix
+
+# Convertit les fins de ligne du fichier mvnw en format Unix
+dos2unix /home/vagrant/vagrantApi/mvnw
 
 # Installation de Maven
 sudo apt-get install -y maven
 
 # se positionner dans le dossier de l'application et construire l'application
-cd /home/vagrant/api
-./mvnw clean package
+cd /home/vagrant/vagrantApi
+ ./mvnw clean install
 
-# execution de l'application Spring Boot
-nohup java -jar target/*.jar > /dev/null 2>&1 &
+ # Déploie l'application 
+ ./mvnw spring-boot:run &
+
